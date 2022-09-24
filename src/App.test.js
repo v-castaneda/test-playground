fd5945100f1bd2ad702bd0f1442ea3b22f06a199
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-09-24 12:19:43 -0400

    added unit tests of header, button, and checkbox

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,35 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test("header renders with correct text", () => {
+  render(<App />)
+  const headerEl = screen.getByRole("heading")
+  expect(headerEl.textContent).toBe("Testing Playground") 
+})
+
+describe('test for the button', () => {
+  test("button changes color when clicked", () => {
+    render(<App />)
+    const colorBtn = screen.getByRole("button")
+  
+    userEvent.click(colorBtn)
+  
+    expect(colorBtn).toHaveStyle({ backgroundColor: "blue" })
+    expect(colorBtn.textContent).toBe("Change button color to black")
+  })
+})
+
+describe('test to check disabling of button with use of checkbox', () => {
+  test('button disabled with checkbox', () => {
+    render(<App />)
+    const colorBtn = screen.getByRole("button")
+    const checkbox = screen.getByRole("checkbox")
+
+    userEvent.click(checkbox)
+    expect(colorBtn).toBeDisabled()
+
+    userEvent.click(checkbox)
+    expect(colorBtn).toBeEnabled()
+  })
+})
